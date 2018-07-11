e3dcd3b04e8e583c2c5c385eb31cddaee1fa75ed
gaboesquivel <contact@gaboesquivel.com>
2018-07-11 17:49:54 -0600

    add attendants list functionality

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
+;(function ($, Handlebars) {
+  var attendandsUrl = 'data/attendants.json'
+  var mentorTemplate = Handlebars.compile($('#attendant-template').html())
 
+  $.getJSON(attendandsUrl)
+    .then(function (attendands) {
+      displayAttendants(attendands)
+    }, handleError)
+
+  function handleError (err) {
+    $('#attendands-container').html('this is really emabarring! something went wrong, we are not able to load the attedants list at this time, try reloading, if it doesn\'t work please contact us')
+    console.error('cannot obtain organizers and attendands data \n', err)
+  }
+
+  function displayAttendants (organizersAndMentors) {
+    var html = ''
+    $.each(organizersAndMentors, function (person, personData) {
+      html += mentorTemplate(personData)
+    })
+    $('#attendants-container').html(html)
+  }
+})(window.jQuery, window.Handlebars)
